4ef5d2db65cdf9090ec696262c8bd8eb50900560
Dhanush <dhanushpamulapati@gmail.com>
2026-06-25 10:27:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    private static boolean checkCommand(String command){
+        return false;
+    }   
+
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        if(!checkCommand(command)){
+            System.out.print(command + ": command not found");
+        }
+        scanner.close();
     }
 }
